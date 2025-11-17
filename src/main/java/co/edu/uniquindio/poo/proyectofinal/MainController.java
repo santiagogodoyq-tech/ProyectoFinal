d95ec7f575b4133ee941febf37ed3a9e90b142c8
santiagogodoyq-tech <santiago.godoyq@uqvirtual.edu.co>
2025-11-17 15:37:16 -0500

    si

--- src/main/java/co/edu/uniquindio/poo/proyectofinal/MainController.java
+++ src/main/java/co/edu/uniquindio/poo/proyectofinal/MainController.java
@@ -3,6 +3,8 @@ package co.edu.uniquindio.poo.proyectofinal;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import javafx.scene.control.Button;
 
 import java.io.IOException;
@@ -14,18 +16,20 @@ public class MainController {
 
     @FXML
     public void initialize() {
+
         btnRegistrarCliente.setOnAction(e -> {
             try {
                 abrirRegistrarCliente();
             } catch (IOException ex) {
-                throw new RuntimeException(ex);
+                ex.printStackTrace();
             }
         });
+
         btnRegistradoCliente.setOnAction(e -> {
             try {
                 abrirConsultar();
             } catch (IOException ex) {
-                throw new RuntimeException(ex);
+                ex.printStackTrace();
             }
         });
     }
@@ -34,11 +38,17 @@ public class MainController {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("client-register.fxml"));
         Parent root = loader.load();
 
+        Stage stage = (Stage) btnRegistrarCliente.getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 
     private void abrirConsultar() throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml"));
         Parent root = loader.load();
+
+        Stage stage = (Stage) btnRegistradoCliente.getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
-
